9b86c7042063732623c5369d1644b343f55cd6ad
rjablock <129575706+rjablock@users.noreply.github.com>
2023-04-20 20:49:27 +0000

    [Update] Railsチュートリアル4章途中

---
+++ try1.rb
@@ -0,0 +1,71 @@
+# city = "Suginami"
+# prefecture = "Tokyo"
+
+# puts city + ' ' + prefecture
+# puts "#{city} #{prefecture}"
+
+
+# puts "racecar".length
+# puts "racecar".reverse
+
+# s = "racecar"
+
+# puts s == s.reverse
+
+# s = "onomatopoeia"
+# puts "It's a palindrome!" if s == s.reverse
+
+ # def palindrome_tester(s)
+ #   if s == s.reverse
+ #     puts "It's a palindrome!"
+ #   else
+ #     puts "It's not a palindrome."
+ #   end
+ # end
+ 
+ # puts palindrome_tester("racecar").nil?
+ # puts palindrome_tester("onomatopoeia")
+ 
+ 
+ # a = "A man, a plan, a canal, Panama".split(',')
+ # s = a.join
+ # s = s.split(' ').join
+ 
+ # puts s == s.reverse
+ # puts s.downcase == s.downcase.reverse
+ 
+ 
+ # a = ("a".."z").to_a
+ # puts a[-7]
+ 
+ 
+ #暗証番号自動生成用
+ # a = ("a".."z").to_a
+ # b = (0..9).to_a
+ # puts (a + b).shuffle[0..18].join
+ 
+ 
+# (0..16).each do |n|
+#  puts n**2
+# end
+
+# def yeller(input_lines)
+#  puts input_lines.join.chomp.upcase
+# end
+
+# input_lines = readlines.map(&:chomp)
+# yeller(input_lines)
+
+# def random_subdomain
+#  n = ("a".."z").to_a.shuffle[0..7].join
+# end
+
+# puts random_subdomain
+
+# def string_shuffle(s)
+#  s.split('').shuffle.join
+# end
+
+# puts string_shuffle("フシギダネ　ヒトカゲ　ゼニガメ")
+
+
