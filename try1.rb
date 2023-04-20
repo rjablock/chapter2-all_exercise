8955e1e281f844e147b61aec1a59f4c5582f2c9f
rjablock <129575706+rjablock@users.noreply.github.com>
2023-04-20 21:41:52 +0000

    [Update] Railsチュートリアル4章途中

--- try1.rb
+++ try1.rb
@@ -69,8 +69,42 @@
 # puts string_shuffle("フシギダネ　ヒトカゲ　ゼニガメ")
 
 
-num = { :one => "uno", :two => "dos", :three => "tres" }
+# num = { :one => "uno", :two => "dos", :three => "tres" }
 
-num.each do |key, value|
- puts "'#{key}'はスペイン語で'#{value}'"
-end
+# num.each do |key, value|
+#  puts "'#{key}'はスペイン語で'#{value}'"
+# end
+
+
+# person1 = { :first => "Cyan", :name => "Hijirikawa" }
+# person2 = { :first => "Dorothy", :name => "Franks" }
+# person3 = { :first => "Elphelt", :name => "Valentine" }
+
+# params = {}
+
+# params[:father] = person1
+# params[:mother] = person2
+# params[:child] = person3
+
+# puts params[:mother][:first]
+
+
+# #下記と出力結果は同じになる
+# user = { :name => "わんごろう", :email => "aaaaaa@gmail.com", :password_digest => ("a".."z").to_a.shuffle[0..15].join }
+
+# puts user[:password_digest]
+
+# #上記と出力結果は同じになる
+# params = {}
+# params[:user] = { :name => "わんごろう", :email => "aaaaaa@gmail.com", :password_digest => ("a".."z").to_a.shuffle[0..15].join }
+
+# puts params[:user][:password_digest]
+
+
+a = 1..10
+puts a.class
+
+b = Range.new(1,10)
+puts b
+
+puts a == b
